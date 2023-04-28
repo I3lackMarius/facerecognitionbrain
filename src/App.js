f9d2f8fd2382c2a1548400abc1df79b7ebd12691
I3lackMarius <1993.marius@gmail.com>
2023-04-28 11:39:48 +0200

    Integrated the face recognition api and tested successfully with only one face per image

--- src/App.js
+++ src/App.js
@@ -6,39 +6,34 @@ import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "./components/Particles/Particles";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import "./App.css";
-
-const raw = JSON.stringify({
-  user_app_id: {
-    user_id: "clarifai",
-    app_id: "main",
-  },
-  inputs: [
-    {
-      data: {
-        image: {
-          url: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1024-512,f_auto,q_auto:best/msnbc/2016_04/434516/gettyimages-507136500.jpg",
-        },
-      },
-    },
-  ],
-});
-const requestOptions = {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    Authorization: "Key b35f7987efab4cf0ba49cd2dd140b32f",
-  },
-  body: raw,
-};
-
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: "",
       imageUrl: "",
+      box: {},
     };
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      JSON.parse(data).outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
   };
@@ -50,10 +45,34 @@ class App extends Component {
     // this will default to the latest version_id
     fetch(
       `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-      requestOptions
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          Authorization: "Key b35f7987efab4cf0ba49cd2dd140b32f",
+        },
+        body: JSON.stringify({
+          user_app_id: {
+            user_id: "clarifai",
+            app_id: "main",
+          },
+          inputs: [
+            {
+              data: {
+                image: {
+                  url: this.state.input, //"https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow.jpg",
+                },
+              },
+            },
+          ],
+        }),
+      }
     )
       .then((response) => response.text())
-      .then((result) => console.log(result))
+      .then((response) => {
+        this.displayFaceBox(this.calculateFaceLocation(response));
+      })
+      //.then((result) => console.log("result", result))
       .catch((error) => console.log("error", error));
   };
   render() {
@@ -67,7 +86,7 @@ class App extends Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
