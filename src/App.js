2506430977867602d9745d787d24516bc81a950d
I3lackMarius <1993.marius@gmail.com>
2023-04-28 08:46:18 +0200

    integrated the face detection api and tested ok, but not configured yet

--- src/App.js
+++ src/App.js
@@ -2,10 +2,60 @@ import React, { Component } from "react";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Particles from "./components/Particles/Particles";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import "./App.css";
+
+const raw = JSON.stringify({
+  user_app_id: {
+    user_id: "clarifai",
+    app_id: "main",
+  },
+  inputs: [
+    {
+      data: {
+        image: {
+          url: "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1024-512,f_auto,q_auto:best/msnbc/2016_04/434516/gettyimages-507136500.jpg",
+        },
+      },
+    },
+  ],
+});
+const requestOptions = {
+  method: "POST",
+  headers: {
+    Accept: "application/json",
+    Authorization: "Key b35f7987efab4cf0ba49cd2dd140b32f",
+  },
+  body: raw,
+};
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+    };
+  }
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+
+    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+    // this will default to the latest version_id
+    fetch(
+      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  };
   render() {
     return (
       <div className="App">
@@ -13,9 +63,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* {
-        <FaceRecognition />} */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
