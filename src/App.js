702108812f8c3c750b70109cff145f0e2f2933a0
I3lackMarius <1993.marius@gmail.com>
2023-05-01 14:03:23 +0200

    moved the clarifai api call to the backend to protect the api key

--- src/App.js
+++ src/App.js
@@ -61,10 +61,11 @@ class App extends Component {
 
   calculateFaceLocation = (data) => {
     const clarifaiFace =
-      JSON.parse(data).outputs[0].data.regions[0].region_info.bounding_box;
+      data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
+    // console.log("calculateFaceLocation", data);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
@@ -82,37 +83,21 @@ class App extends Component {
   };
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input });
-
     // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
     // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
     // this will default to the latest version_id
-    fetch(
-      `https://api.clarifai.com/v2/models/face-detection/versions/6dc7e46bc9124c5c8824be4822abe105/outputs`,
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          Authorization: "Key b35f7987efab4cf0ba49cd2dd140b32f",
-        },
-        body: JSON.stringify({
-          user_app_id: {
-            user_id: "clarifai",
-            app_id: "main",
-          },
-          inputs: [
-            {
-              data: {
-                image: {
-                  url: this.state.input, //"https://images.healthshots.com/healthshots/en/uploads/2023/01/18175821/face-glow.jpg",
-                },
-              },
-            },
-          ],
-        }),
-      }
-    )
-      .then((response) => response.text())
+    fetch("http://localhost:3000/imageUrl", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
       .then((response) => {
+        // console.log(response);
         if (response) {
           fetch("http://localhost:3000/image", {
             method: "PUT",
